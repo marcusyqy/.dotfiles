2a866498c56ccc78a2ebb93dcd361baaf223df4c
marcusyqy <marcusyqy@gmail.com>
2021-09-02 01:48:54 +0800

    commited fish shell - cant decide what to use

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+
+alias lg="lazygit"
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 install_vim_config() {
     rm -rf ~/.config/nvim
@@ -14,7 +15,13 @@ install_zsh_config() {
     cp $DOTFILES/zsh/.zshrc ~/.zshrc
 }
 
+install_fish_config() {
+    rm -rf ~/.config/fish
+    cp -r $DOTFILES/fish/ ~/.config/fish
+}
+
 
 install_vim_config
 install_tmux_config
-install_zsh_config
+install_fish_config
+#install_zsh_config
