73dc77c8f5be930a83dcd2a768e5f52db427e57b
marcusyqy <marcusyqy@gmail.com>
2021-11-01 10:02:59 +0800

    screenlayout

--- .screenlayout/screenlayout.sh
+++ .screenlayout/screenlayout.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 #xrandr --output DP-1 --off --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output eDP-1 --off --output HDMI-2 --off
+dp0connected=`xrandr | grep 'DP-0' | grep -c ' connected'`
+dp1connected=`xrandr | grep -sw 'DP-1' | grep -c ' connected'`
 
+echo "$dp0connected"
+echo "$dp1connected"
 sleep .1
-if xrandr | grep 'DP-0' | grep ' connected' &>/dev/null; then
+if [ $dp0connected -eq 1 ]
+then
   echo 'DP-0 connected, restoring layout'
   xrandr --output DP-0 --primary --mode 3440x1440 --pos 0x0 --rate 144.0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output eDP-1-1 --off
+elif [ $dp1connected -eq 1 ]
+then
+  echo 'DP-1 connected, restoring layout'
+  xrandr --output DP-1 --primary --mode 3440x1440 --pos 0x0 --rate 144.0 --rotate normal --output DP-0 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output eDP-1-1 --off
 else
-  echo 'No DP-0 detected, reverting to laptop display'
+  echo 'No DP-0/DP-1 detected, reverting to laptop display'
   xrandr --output DP-1 --off --output HDMI-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off
 fi
 
