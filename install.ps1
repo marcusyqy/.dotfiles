2423602bf52f1b14f98b803e37b611dc93495cd2
Marcus Yong <marcusyqy@gmail.com>
2021-12-28 00:30:47 +0800

    added windows build for alacritty hopefully it works

--- install.ps1
+++ install.ps1
@@ -9,7 +9,13 @@ function update_ps {
     Copy-Item -Force powershell/Microsoft.PowerShell_profile.ps1 $PROFILE
 }
 
+function update_alacritty {
+    New-Item -Force -Type Directory $env:APPDATA/alacritty
+    Copy-Item -Force -Recurse alacritty/* $env:APPDATA/alacritty
+}
+
 update_vim
 update_ps
+update_alacritty
 
 
