1e8c9f5a1d95abb23c829076828a527fea10464a
Marcus Yong <marcusyqy@gmail.com>
2022-04-06 23:32:08 +0800

    renamed to luasnip.lua

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -4,8 +4,6 @@ lua << EOF
 --   return
 -- end
 
-local ls = require "luasnip"
-
 ls.snippets = {
     all = {
         ls.parser.parse_snippet("atask", "[$1]\ncommand=$2\noutput=terminal"),
@@ -14,7 +12,8 @@ ls.snippets = {
 
 -- <c-k> is my expansion key
 -- this will expand the current item or jump to the next item within the snippet.
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
+vim.api.nvim_set_keymap({ "i", "s" }, "<c-k>", function()
+-- vim.keymap.set({ "i", "s" }, "<c-k>", function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
@@ -22,7 +21,8 @@ end, { silent = true })
 
 -- <c-j> is my jump backwards key.
 -- this always moves to the previous item within the snippet
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
+vim.api.nvim_set_keymap({ "i", "s" }, "<c-j>", function()
+--vim.keymap.set({ "i", "s" }, "<c-j>", function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
@@ -30,11 +30,10 @@ end, { silent = true })
 
 -- <c-l> is selecting within a list of options.
 -- This is useful for choice nodes (introduced in the forthcoming episode 2)
-vim.keymap.set("i", "<c-l>", function()
+vim.api.nvim_set_keymap("i", "<c-l>", function()
+--vim.keymap.set("i", "<c-l>", function()
   if ls.choice_active() then
     ls.change_choice(1)
   end
 end)
-
-EOF
 
