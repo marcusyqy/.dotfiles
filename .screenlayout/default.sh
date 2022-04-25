aecadd98d814aa28a27a8ac886f9dd7f0e4a4b51
marcus.yong <marcus.yong@motional.com>
2022-04-25 14:34:17 +0800

    yay

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-#turn off current laptop screen and turn on another screen :)
-xrandr --auto && xrandr --output eDP-1 --off && xrandr --output $(xrandr | grep " connected " | grep -v "eDP-1" | awk '{print$1}') --primary
+non_connected_laptops=`xrandr | grep " connected " | grep -v "eDP-1" | awk '{print$1}'`
+if [ -z "$non_connected_laptops" ]
+then
+    xrandr --auto
+else
+    #turn off current laptop screen and turn on another screen :)
+    xrandr --auto && xrandr --output eDP-1 --off && xrandr --output $(xrandr | grep " connected " | grep -v "eDP-1" | awk '{print$1}') --primary
+fi
+
