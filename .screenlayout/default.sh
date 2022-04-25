ac3111107fce31a786b0e6361a856e47ca0fedc6
marcus.yong <marcus.yong@motional.com>
2022-04-25 14:22:12 +0800

    default
    a

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #turn off current laptop screen and turn on another screen :)
-xrandr --auto && xrandr --output eDP-1 --off
+xrandr --auto && xrandr --output eDP-1 --off && xrandr --output $(xrandr | grep " connected " | grep -v "eDP-1" | awk '{print$1}') --primary
