49f53ed0067fd2de44310a00d491a4dfa10c0780
Marcus Yong <marcusyqy@gmail.com>
2022-06-09 09:49:27 +0800

    played around a little with picom and awesome

--- awesome/theme/default/theme.lua
+++ awesome/theme/default/theme.lua
@@ -25,9 +25,9 @@ theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
 theme.useless_gap   = dpi(4)
-theme.border_width  = dpi(1)
-theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
+theme.border_width  = dpi(1.5)
+theme.border_normal = "#535d6c"
+theme.border_focus  = "#ebdbb2"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
