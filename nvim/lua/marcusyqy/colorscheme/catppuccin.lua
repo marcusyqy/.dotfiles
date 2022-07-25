28a205849ebc9e89fe09b41115f0f6404295af8b
Marcus Yong <marcus.yong@motional.com>
2022-07-25 10:51:12 +0800

    nice

---
+++ nvim/lua/marcusyqy/colorscheme/catppuccin.lua
@@ -0,0 +1,80 @@
+
+local catppuccin = require("catppuccin")
+local settings = {
+    transparent_background = false,
+    term_colors = false,
+    styles = {
+        comments = "italic",
+        conditionals = "NONE",
+        loops = "NONE",
+        functions = "bold",
+        keywords = "italic",
+        strings = "NONE",
+        variables = "NONE",
+        numbers = "italic",
+        booleans = "italic",
+        properties = "NONE",
+        types = "italic",
+        operators = "NONE",
+    },
+    integrations = {
+        treesitter = true,
+        native_lsp = {
+        enabled = true,
+        virtual_text = {
+            errors = "bold,italic",
+            hints = "italic",
+            warnings = "italic",
+            information = "italic",
+        },
+        underlines = {
+            errors = "underline",
+            hints = "underline",
+            warnings = "underline",
+            information = "underline",
+        },
+    },
+    coc_nvim = false,
+    lsp_trouble = false,
+    cmp = true,
+    lsp_saga = false,
+    gitgutter = false,
+    gitsigns = true,
+    telescope = true,
+    nvimtree = {
+        enabled = true,
+        show_root = false,
+        transparent_panel = true,
+    },
+    neotree = {
+        enabled = false,
+        show_root = false,
+        transparent_panel = true,
+    },
+    which_key = false,
+    indent_blankline = {
+        enabled = true,
+        colored_indent_levels = false,
+    },
+    dashboard = true,
+    neogit = false,
+    vim_sneak = false,
+    fern = false,
+    barbar = false,
+    bufferline = true,
+    markdown = true,
+    lightspeed = false,
+    ts_rainbow = false,
+    hop = false,
+    notify = true,
+    telekasten = true,
+    symbols_outline = true,
+}
+}
+print("hello")
+-- configure it
+catppuccin.setup(settings)
+
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+vim.g.background = "dark"
+vim.cmd("colorscheme catppuccin")
---
+++ nvim/lua/marcusyqy/colorscheme/tokyonight.lua
@@ -0,0 +1,18 @@
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_transparent = true
+vim.g.background = "dark"
+
+vim.g.tokyonight_italic_comments	= true	-- Make comments italic
+vim.g.tokyonight_italic_keywords	= true	-- Make keywords italic
+vim.g.tokyonight_italic_variables	= false	-- Make variables and identifiers italic,
+vim.g.tokyonight_italic_functions	= false	-- Make functions italic
+vim.g.tokyonight_dark_float	= true	-- Float windows like the lsp diagnostics windows get a darker background.
+vim.g.tokyonight_lualine_bold	= true
+vim.g.tokyonight_hide_inactive_statusline = false-- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard StatusLine and LuaLine.
+
+-- allow telescope to be transparent
+vim.g.tokyonight_colors = {
+    bg_float = "none"
+}
+
+vim.cmd("colorscheme tokyonight")
