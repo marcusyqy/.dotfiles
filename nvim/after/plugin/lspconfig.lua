45c6e9b4c17ec743be918e18cde1b681f5e3031b
Marcus Yong <marcusyqy@gmail.com>
2022-08-04 13:52:19 +0800

    add python

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -198,7 +198,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 -- "cssls", "tailwindcss",
-local servers = { "svelte", "gopls" }
+local servers = { "svelte", "gopls", "pylsp" }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
         on_attach = on_attach,
