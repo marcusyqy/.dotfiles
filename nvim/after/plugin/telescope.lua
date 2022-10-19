817899e8cc685f63d45b34c90a083f112873e7b7
Marcus Yong <marcus.yong@motional.com>
2022-10-19 16:07:05 +0800

    change telescope back

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -24,143 +24,157 @@ require("telescope").load_extension("git_worktree")
 
 local actions = require("telescope.actions")
 
--- require('telescope').setup{
---   defaults = {
---     -- Default configuration for telescope goes here:
---     -- config_key = value,
---     mappings = {
---       n = {
---           ['<c-d>'] = require('telescope.actions').delete_buffer
---       }, -- n
---       i = {
---         ["<C-h>"] = "which_key",
---         ['<c-d>'] = require('telescope.actions').delete_buffer
---       } -- i
---     } -- mappings
---   }, -- defaults
--- } -- telescope setup
 
-local options = {
-   defaults = {
-      vimgrep_arguments = {
-         "rg",
-         "--color=never",
-         "--no-heading",
-         "--with-filename",
-         "--line-number",
-         "--column",
-         "--smart-case",
-      },
-      prompt_prefix = "   ",
-      selection_caret = "  ",
-      entry_prefix = "  ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      sorting_strategy = "ascending",
-      layout_strategy = "horizontal",
-      layout_config = {
-         horizontal = {
-            prompt_position = "top",
-            preview_width = 0.55,
-            results_width = 0.8,
-         },
-         vertical = {
-            mirror = false,
-         },
-         width = 0.87,
-         height = 0.80,
-         preview_cutoff = 20,
-      },
-      file_sorter = require("telescope.sorters").get_fuzzy_file,
-      file_ignore_patterns = {
-        "vendor/*",
-        "%.lock",
-        "__pycache__/*",
-        "%.sqlite3",
-        "%.ipynb",
-        "node_modules/*",
-        "%.jpg",
-        "%.jpeg",
-        "%.png",
-        "%.svg",
-        "%.otf",
-        "%.ttf",
-        ".git/",
-        "%.webp",
-        ".dart_tool/",
-        ".github/",
-        ".gradle/",
-        ".idea/",
-        ".settings/",
-        ".vscode/",
-        "__pycache__/",
-        "build/",
-        "env/",
-        "gradle/",
-        "node_modules/",
-        "target/",
-        "%.pdb",
-        "%.dll",
-        "%.class",
-        "%.exe",
-        "%.cache",
-        "%.ico",
-        "%.pdf",
-        "%.dylib",
-        "%.jar",
-        "%.docx",
-        "%.met",
-        "smalljre_*/*",
-        ".vale/",
-      },
-      generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-      path_display = { "truncate" },
-      winblend = 0,
-      border = {},
-      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-      -- borderchars = {
-      --   prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
-      --   results = { " " },
-      --   preview = { " " },
-      -- },
-      color_devicons = true,
-      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-      -- Developer configurations: Not meant for general override
-      buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-      mappings = {
-         n = { ["q"] = require("telescope.actions").close },
-      },
-   },
-   preview = {
-       filesize_hook = function(filepath, bufnr, opts)
-           local max_bytes = 10000
-           local cmd = {"head", "-c", max_bytes, filepath}
-           require('telescope.previewers.utils').job_maker(cmd, bufnr, opts)
-       end
-   },
-   extensions_list = { "themes", "terms"},
-   extensions = {
-       fzf = {
-            fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = true,  -- override the generic sorter
-            override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                               -- the default case_mode is "smart_case"
-       }
-  }
-}
+-- local options = {
+--    defaults = {
+--       vimgrep_arguments = {
+--          "rg",
+--          "--color=never",
+--          "--no-heading",
+--          "--with-filename",
+--          "--line-number",
+--          "--column",
+--          "--smart-case",
+--       },
+--       prompt_prefix = "   ",
+--       selection_caret = "  ",
+--       entry_prefix = "  ",
+--       initial_mode = "insert",
+--       selection_strategy = "reset",
+--       sorting_strategy = "ascending",
+--       layout_strategy = "horizontal",
+--       layout_config = {
+--          horizontal = {
+--             prompt_position = "top",
+--             preview_width = 0.55,
+--             results_width = 0.8,
+--          },
+--          vertical = {
+--             mirror = false,
+--          },
+--          width = 0.87,
+--          height = 0.80,
+--          preview_cutoff = 20,
+--       },
+--       file_sorter = require("telescope.sorters").get_fuzzy_file,
+--       file_ignore_patterns = {
+--         "vendor/*",
+--         "%.lock",
+--         "__pycache__/*",
+--         "%.sqlite3",
+--         "%.ipynb",
+--         "node_modules/*",
+--         "%.jpg",
+--         "%.jpeg",
+--         "%.png",
+--         "%.svg",
+--         "%.otf",
+--         "%.ttf",
+--         ".git/",
+--         "%.webp",
+--         ".dart_tool/",
+--         ".github/",
+--         ".gradle/",
+--         ".idea/",
+--         ".settings/",
+--         ".vscode/",
+--         "__pycache__/",
+--         "build/",
+--         "env/",
+--         "gradle/",
+--         "node_modules/",
+--         "target/",
+--         "%.pdb",
+--         "%.dll",
+--         "%.class",
+--         "%.exe",
+--         "%.cache",
+--         "%.ico",
+--         "%.pdf",
+--         "%.dylib",
+--         "%.jar",
+--         "%.docx",
+--         "%.met",
+--         "smalljre_*/*",
+--         ".vale/",
+--       },
+--       generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+--       path_display = { "truncate" },
+--       winblend = 0,
+--       border = {},
+--       borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+--       -- borderchars = {
+--       --   prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
+--       --   results = { " " },
+--       --   preview = { " " },
+--       -- },
+--       color_devicons = true,
+--       set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+--       file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+--       grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+--       qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+--       -- Developer configurations: Not meant for general override
+--       buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+--       mappings = {
+--          n = { ["q"] = require("telescope.actions").close },
+--       },
+--    },
+--    preview = {
+--        filesize_hook = function(filepath, bufnr, opts)
+--            local max_bytes = 10000
+--            local cmd = {"head", "-c", max_bytes, filepath}
+--            require('telescope.previewers.utils').job_maker(cmd, bufnr, opts)
+--        end
+--    },
+--    extensions_list = { "themes", "terms"},
+--    extensions = {
+--        fzf = {
+--             fuzzy = true,                    -- false will only do exact matching
+--             override_generic_sorter = true,  -- override the generic sorter
+--             override_file_sorter = true,     -- override the file sorter
+--             case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+--                                                -- the default case_mode is "smart_case"
+--        }
+--   }
+-- }
+-- require("telescope").setup(options)
+--
+-- -- load extensions
+-- pcall(function()
+--    for _, ext in ipairs(options.extensions_list) do
+--       require("telescope").load_extension(ext)
+--    end
+-- end)
 
-require("telescope").setup(options)
+require("telescope").setup({
+	defaults = {
+		file_sorter = require("telescope.sorters").get_fzy_sorter,
+		-- prompt_prefix = " >",
+        prompt_prefix = "   ",
+		color_devicons = true,
 
--- load extensions
-pcall(function()
-   for _, ext in ipairs(options.extensions_list) do
-      require("telescope").load_extension(ext)
-   end
-end)
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+
+		mappings = {
+			i = {
+				["<C-x>"] = false,
+				["<C-q>"] = actions.send_to_qflist,
+			},
+		},
+	},
+    --[[
+	extensions = {
+		fzy_native = {
+			override_generic_sorter = false,
+			override_file_sorter = true,
+		},
+	},
+    ]]
+})
+
+
 
 
 
@@ -183,9 +197,12 @@ local function git_branch_private()
 nnoremap("<leader>ps", function() require('telescope.builtin').grep_string({search = vim.fn.input("Find For > "), previewer=false}) end);
 
 -- Find files using Telescope command-line sugar.
-nnoremap("<c-p>", function() require('telescope.builtin').git_files({previewer=false}) end)
-nnoremap("<leader>ff", function() require('telescope.builtin').find_files({previewer=false}) end)
-nnoremap("<leader>eps", function() require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown({previewer=false})) end)
+
+-- local telescope_opts = { previewer = false }
+local telescope_opts = {}
+nnoremap("<c-p>", function() require('telescope.builtin').git_files(telescope_opts) end)
+nnoremap("<leader>ff", function() require('telescope.builtin').find_files(telescope_opts) end)
+nnoremap("<leader>eps", function() require('telescope.builtin').live_grep(require('telescope.themes').get_dropdown(telescope_opts)) end)
 nnoremap("<leader>fb", function() require('telescope.builtin').buffers(require('telescope.themes').get_ivy({})) end)
 nnoremap("<leader>fh", function() require('telescope.builtin').help_tags(require('telescope.themes').get_ivy({})) end)
 nnoremap("<leader>fgc", function() require('telescope.builtin').git_commits(require('telescope.themes').get_dropdown({})) end)
@@ -194,6 +211,6 @@ nnoremap("<leader>cmd", function() require('telescope.builtin').commands() end)
 -- nnoremap <leader>pp", <cmd>lua require'telescope'.extensions.project.project{}<cr>
 
 -- test for building
-nnoremap("<leader>to", function() require('telescope').extensions.asynctasks.all(require('telescope.themes').get_dropdown({previewer=false})) end)
+nnoremap("<leader>to", function() require('telescope').extensions.asynctasks.all(require('telescope.themes').get_dropdown(telescope_opts)) end)
 nnoremap("<leader>te", vimfn([[e .tasks]]))
 
