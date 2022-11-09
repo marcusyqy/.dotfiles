5ef9eb9733792e2ffb74ef1c0bed830c63b5672f
Marcus Yong <marcus.yong@motional.com>
2022-11-09 20:32:16 +0800

    h

--- nvim/lua/marcusyqy/packer.lua
+++ nvim/lua/marcusyqy/packer.lua
@@ -62,9 +62,11 @@ return require('packer').startup(function()
     }
 
     -- Post-install/update hook with neovim command
-    use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-    use 'nvim-treesitter/nvim-treesitter-context'
-    use 'nvim-treesitter/playground'
+    use {
+        { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
+        'nvim-treesitter/nvim-treesitter-context',
+        'nvim-treesitter/playground'
+    }
 
 
 
