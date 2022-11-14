88401b2bb4df26e5576da58b2ef47d898eb14b1c
Marcus Yong <marcus.yong@motional.com>
2022-11-14 13:23:28 +0800

    yey

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -131,11 +131,11 @@ nnoremap("gp", vimfn([[Lspsaga preview_definition]]), { silent = true,noremap = 
 nnoremap("gr", vimfn([[Lspsaga rename]]), { silent = true,noremap = true })
 
 -- jump and show diagnostic (try)
-nnoremap("<leader>gh", vimfn([[Lspsaga show_line_diagnostics]]), { silent = true,noremap = true })
-vnoremap("<leader>gh", vimfn([[Lspsaga show_line_diagnostics]]), { silent = true,noremap = true })
+nnoremap("gh", vimfn([[Lspsaga show_line_diagnostics]]), { silent = true,noremap = true })
+vnoremap("gh", vimfn([[Lspsaga show_line_diagnostics]]), { silent = true,noremap = true })
 
 -- jump diagnostic
-nnoremap("<leader>gk", vimfn([[Lspsaga diagnostic_jump_prev]]), { silent = true, noremap =true })
-nnoremap("<leader>gj", vimfn([[Lspsaga diagnostic_jump_next]]), { silent = true, noremap =true })
+nnoremap("<c-k>", vimfn([[Lspsaga diagnostic_jump_prev]]), { silent = true, noremap =true })
+nnoremap("<c-j>", vimfn([[Lspsaga diagnostic_jump_next]]), { silent = true, noremap =true })
 -- or use command
 
--- nvim/lua/marcusyqy/navigation.lua
+++ nvim/lua/marcusyqy/navigation.lua
@@ -2,8 +2,8 @@
 local Remaps   = require("marcusyqy.keymap")
 local nnoremap = Remaps.nnoremap
 
-nnoremap("<C-j>", ":cnext<CR>zz")
-nnoremap("<C-k>", ":cprev<CR>zz")
+nnoremap("<leader>gj", ":cnext<CR>zz")
+nnoremap("<leader>gk", ":cprev<CR>zz")
 nnoremap("<leader>j", ":lnext<CR>zz")
 nnoremap("<leader>k", ":lprev<CR>zz")
 
