6f608ef0078483ae9fbb154c444951025053f52c
Marcus Yong <marcus.yong@motional.com>
2022-11-25 14:16:28 +0800

    setstststs

--- nvim/lua/marcusyqy/set.lua
+++ nvim/lua/marcusyqy/set.lua
@@ -33,13 +33,15 @@ vim.opt.smartcase = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 
-if vim.fn.has("win32") ==1 then
-    vim.cmd([[set undodir="~/.vim/undodir"]])
-else
-    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-end
+vim.opt.undodir = vim.fn.expand('~/.vim/undodir/')
 
+-- if vim.fn.has("win32") ==1 then
+--     vim.cmd([[set undodir="~/.vim/undodir"]])
+-- else
+--     vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- end
 -- vim.opt.undodir = "~/.vim/undodir"
+
 vim.opt.undofile = true
 vim.opt.incsearch = true
 
