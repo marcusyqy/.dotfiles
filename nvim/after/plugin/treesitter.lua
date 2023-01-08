af6f71c1f08bf5560535fd41776496c31219284a
Marcus Yong <marcusyqy@gmail.com>
2023-01-08 23:29:55 +0800

    ts-autotag

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,38 +1,50 @@
 if not vim.g.loaded_nvim_treesitter then
-  print("Treesitter not loaded")
-  return
+    print("Treesitter not loaded")
+    return
 end
 
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = false,
-    disable = {},
-  },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
+require 'nvim-treesitter.configs'.setup {
+    highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false,
     },
-  },
-  sync_install = false,
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'dockerfile', 'tsx', 'html', 'json' },
+    indent = {
+        enable = false,
+        disable = {},
+    },
+    playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+            toggle_query_editor = 'o',
+            toggle_hl_groups = 'i',
+            toggle_injected_languages = 't',
+            toggle_anonymous_nodes = 'a',
+            toggle_language_display = 'I',
+            focus_language = 'f',
+            unfocus_language = 'F',
+            update = 'R',
+            goto_node = '<cr>',
+            show_help = '?',
+        },
+    },
+    autotag = {
+        enable = true,
+        filetypes = {
+            'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx',
+            'rescript',
+            'xml',
+            'php',
+            'markdown',
+            'glimmer', 'handlebars', 'hbs'
+        },
+    },
+    sync_install = false,
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'dockerfile', 'tsx', 'html',
+        'json' },
 }
 
 -- ensure_installed = "all"
@@ -41,22 +53,20 @@ require'nvim-treesitter.configs'.setup {
 -- parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
 
 -- {
-            --"lua",
-            -- "tsx",
-            -- "toml",
-            -- "fish",
-            -- "php",
-            -- "json",
-            -- "yaml",
-            -- "html",
-            -- "javascript",
-            -- "lua",
-            -- "scss",
-            -- "c",
-            -- "dockerfile",
-            -- "rust",
-            -- "cpp"
-            -- },
+--"lua",
+-- "tsx",
+-- "toml",
+-- "fish",
+-- "php",
+-- "json",
+-- "yaml",
+-- "html",
+-- "javascript",
+-- "lua",
+-- "scss",
+-- "c",
+-- "dockerfile",
+-- "rust",
+-- "cpp"
+-- },
 -- }
-
-
