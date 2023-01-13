e45914db1e110b414f05cec3b5796854b515dd2a
Marcus Yong <marcus.yong@motional.com>
2023-01-13 14:21:38 +0800

    alacritty

--- wallpapers/scripts/random.sh
+++ wallpapers/scripts/random.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-feh --bg-fill --randomize ~/wallpapers/images/
+# feh --bg-fill --randomize ~/wallpapers/images/
+feh --bg-fill --randomize ~/wallpapers/nord/
+# feh --bg-fill --randomize ~/wallpapers/anime/
