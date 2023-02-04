039164510e477ef2b4bd06554124ecf9eef61b0f
Marcus Yong <marcusyqy@gmail.com>
2023-02-05 01:42:56 +0800

    commit changes to neovide

--- nvim/lua/gui/neovide.lua
+++ nvim/lua/gui/neovide.lua
@@ -31,7 +31,6 @@ if vim.g.neovide then
     vim.g.neovide_cursor_animation_length = 0.03
     vim.g.neovide_cursor_trail_size = 0.8
     vim.g.neovide_cursor_antialiasing = true
-    vim.g.neovide_cursor_antialiasing = true
     vim.g.neovide_cursor_vfx_mode = ""
 end
 
