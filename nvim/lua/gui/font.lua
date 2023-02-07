d114dbe2b2d0916a46a55fd07f8f136cc57c7e3b
Marcus Yong <marcusyqy@gmail.com>
2023-02-07 21:47:07 +0800

    font update

--- nvim/lua/gui/font.lua
+++ nvim/lua/gui/font.lua
@@ -3,10 +3,14 @@ local Remaps   = require("marcusyqy.keymap")
 local nnoremap = Remaps.nnoremap
 local inoremap = Remaps.inoremap
 
-local font_size = 14;
+local font_size = 13;
 local function set_font_size(new_font_size)
     font_size = new_font_size
     vim.opt.guifont = { "JetBrainsMono NF", ":h" .. tostring(font_size) }
+end
+
+local function print_front_size()
+    print(font_size)
 end
 
 local function adjust_font_size(delta)
@@ -23,5 +27,6 @@ set_font_size(font_size)
 
 local M = {}
 M.set = set_font_size
+M.print = print_front_size
 return M
 
