f4ab9561d2da0c1bf58258532666da61e64eb0a7
Marcus Yong <marcus.yong@motional.com>
2023-03-25 04:10:35 +0800

    i prefer to have no notifications

--- nvim/after/plugin/notify.lua
+++ nvim/after/plugin/notify.lua
@@ -1,13 +1,13 @@
-local Remap = require("marcusyqy.keymap")
-local nnoremap = Remap.nnoremap
-local vnoremap = Remap.vnoremap
-
-require("notify").setup({ background_colour = "#000000",
-    stages = "fade_in_slide_out"
-})
-vim.notify = require("notify")
-
-
-
-nnoremap("<c-t>", function() require("notify").dismiss() end, { silent = true, noremap = true })
-vnoremap("<c-t>", function() require("notify").dismiss() end, { silent = true, noremap = true })
+-- local Remap = require("marcusyqy.keymap")
+-- local nnoremap = Remap.nnoremap
+-- local vnoremap = Remap.vnoremap
+--
+-- require("notify").setup({ background_colour = "#000000",
+--     stages = "fade_in_slide_out"
+-- })
+-- vim.notify = require("notify")
+--
+--
+--
+-- nnoremap("<c-t>", function() require("notify").dismiss() end, { silent = true, noremap = true })
+-- vnoremap("<c-t>", function() require("notify").dismiss() end, { silent = true, noremap = true })
