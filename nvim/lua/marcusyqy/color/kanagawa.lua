bf82c2dd9df567bab8ef7841b6b0670515f33ceb
Marcus Yong <marcus.yong@motional.com>
2023-05-09 22:21:56 +0800

    kanagawa

--- nvim/lua/marcusyqy/color/kanagawa.lua
+++ nvim/lua/marcusyqy/color/kanagawa.lua
@@ -22,6 +22,8 @@ require('kanagawa').setup({
     overrides = function(colors)
         local theme = colors.theme
         return {
+            -- Assign a static color to strings
+            String = { fg = colors.palette.carpYellow, italic = true },
             NormalFloat = { bg = "none" },
             FloatBorder = { bg = "none" },
             FloatTitle = { bg = "none" },
@@ -35,6 +37,17 @@ require('kanagawa').setup({
             -- set their background accordingly if you wish to keep them dark and borderless
             LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
             MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            TelescopeTitle = { fg = theme.ui.special, bold = true },
+            -- TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            -- TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            -- TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            -- TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            -- TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            -- TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
         }
     end,
     theme = "wave", -- Load "wave" theme when 'background' option is not set
@@ -46,3 +59,4 @@ require('kanagawa').setup({
 
 -- setup must be called before loading
 vim.cmd("colorscheme kanagawa")
+
