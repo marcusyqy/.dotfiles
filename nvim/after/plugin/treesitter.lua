5ce2f6dcccb9198db884f3cd3bd6a3c14fd9270d
Marcus Yong <marcusyqy@gmail.com>
2023-07-24 21:53:46 +0800

    remove indent

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -29,7 +29,7 @@ require 'nvim-treesitter.configs'.setup {
     --     additional_vim_regex_highlighting = false,
     -- },
     indent = {
-        enable = true,
+        enable = false,
         disable = {},
     },
     playground = {
