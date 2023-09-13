ef62e7c83c60650467121c336d2c44f83839f7ac
Marcus Yong <marcusyqy@gmail.com>
2023-09-12 20:31:51 -0400

    lel

--- nvim/lua/marcusyqy/statusline/lualine.lua
+++ nvim/lua/marcusyqy/statusline/lualine.lua
@@ -18,7 +18,7 @@ end
 lualine.setup {
     options = {
         icons_enabled = true,
-        theme = 'auto', --require("marcusyqy.color").colorscheme,
+        theme = 'NeoSolarized', --require("marcusyqy.color").colorscheme,
         -- component_separators = { left = '|', right = '▓'},
         -- section_separators = { left = '▓', right = '▓'},
         component_separators = { left = '', right = ''},
