c747795024fc210f7eb2a00a17e7ca4fcf3f1da9
Marcus Yong <marcusyqy@gmail.com>
2024-03-17 15:28:18 -0400

    lame

--- nvim/lua/gui/nvy.lua
+++ nvim/lua/gui/nvy.lua
@@ -1,4 +1,5 @@
 
 if vim.g.nvy then
-    vim.opt.guifont="ComicMono NF:h10" --  Nerd Font Mono:h10"
+    -- vim.opt.guifont="ComicMono NF:h10" --  Nerd Font Mono:h10"
+    vim.opt.guifont="FiraMono Nerd Font Mono:h10"
 end
--- nvim/lua/marcusyqy/os/windows.lua
+++ nvim/lua/marcusyqy/os/windows.lua
@@ -1,11 +1,11 @@
 -- vim.opt.shell = "pwsh.exe"
-vim.cmd [[
-    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-    let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
-    let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-    let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
-    " set shellcmdflag=-c
-    set shellquote= shellxquote=
-]]
-
-print("windows is called")
+-- vim.cmd [[
+--     let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+--     let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+--     let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+--     let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+--     " set shellcmdflag=-c
+--     set shellquote= shellxquote=
+-- ]]
+--
+-- print("windows is called")
