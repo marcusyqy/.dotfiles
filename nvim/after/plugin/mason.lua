a2c2e5572bebee92f04843fc9bbbe08df6ae38f2
Marcus Yong <marcusyqy@gmail.com>
2024-06-22 20:41:13 -0400

    I like lua

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -3,7 +3,8 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd'  --[[ , 'rust_analyzer', 'pyright', 'tsserver', 'lua_ls', 'gopls', "svelte", "astro", "tailwindcss"  ]]}
+local servers = { "clangd", "lua_ls" } -- , "ocaml-lsp" }
+--[[ , 'rust_analyzer', 'pyright', 'tsserver', 'lua_ls', 'gopls', "svelte", "astro", "tailwindcss"  }]]
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
