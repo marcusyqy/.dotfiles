72551f8c9981ee73cbb99d3370ecf7693a0d3c1d
Marcus Yong <marcusyqy@gmail.com>
2024-08-06 22:33:46 -0400

    Windows change

--- wezterm/platform/windows.lua
+++ wezterm/platform/windows.lua
@@ -7,7 +7,7 @@ local module = {}
 local wezterm = require 'wezterm'
 
 function module.apply_config(config)
-    config.default_cwd = "C:\\Users\\Marcus"
+    -- config.default_cwd = "C:\\Users\\Marcus"
     config.default_prog = { "cmd.exe"
         -- "cmd /k 'C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/VsDevCmd.bat' -startdir=none -arch=x64 -host_arch=x64"
     }
