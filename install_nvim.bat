4b3c2849a3665627fb3bcfcaf1b5bec7ffee23f8
Marcus Yong <marcusyqy@gmail.com>
2024-08-27 21:25:59 -0400

    Cleanup

--- install_nvim.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rd /s /q %userprofile%\AppData\Local\nvim
-echo D | xcopy nvim %userprofile%\AppData\Local\nvim /E /A
-
-rd /s /q %userprofile%\.config\wezterm
-echo D | xcopy wezterm %userprofile%\.config\wezterm /E /A
