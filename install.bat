5f63bb0930d060975fd0cfe054c08cf81ded3eb9
Marcus Yong <marcusyqy@gmail.com>
2024-09-26 00:21:07 -0400

    remove wezterm

--- install.bat
+++ install.bat
@@ -3,7 +3,8 @@
 rd /s /q %userprofile%\AppData\Local\nvim
 echo D | xcopy nvim %userprofile%\AppData\Local\nvim /E /A
 
-echo A | xcopy alacritty %userprofile%\AppData\alacritty\ /E /A
+REM echo A | xcopy alacritty %userprofile%\AppData\alacritty\ /E /A
 
-rd /s /q %userprofile%\.config\wezterm
-echo D | xcopy wezterm %userprofile%\.config\wezterm /E /A
+REM uncomment when you want to update wezterm config
+REM rd /s /q %userprofile%\.config\wezterm
+REM echo D | xcopy wezterm %userprofile%\.config\wezterm /E /A
