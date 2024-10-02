4bb9490e79596f1f061b2d045c4a203adfcc9524
Marcus Yong <marcusyqy@gmail.com>
2024-10-02 15:57:11 -0400

    Nice

--- nvim/colors/vivid-chalk.lua
+++
@@ -1,7 +0,0 @@
-
--- vim.cmd.highlight('clear')
-vim.g.colors_name = 'vim-marcus'
-
-vim.cmd([[colorscheme vividchalk]])
-vim.cmd([[highlight Normal guibg=None]])
-
--- nvim/lua/marcusyqy/color/init.lua
+++ nvim/lua/marcusyqy/color/init.lua
@@ -3,8 +3,11 @@
 -- vim.opt.pumblend = 5
 
 require("marcusyqy.color.gruvbox")
-
 --
 vim.cmd("colorscheme vividchalk")
 vim.cmd("hi Normal guibg=None")
+vim.cmd("hi LineNr guifg=orange guibg=#222222")
+vim.cmd("hi LineNrBelow guifg=white guibg=#222222")
+vim.cmd("hi LineNrAbove guifg=white guibg=#222222")
+
 
