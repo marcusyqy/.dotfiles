066dd80a48c1ba4d4b62ded3c5281a13ea6fda46
Marcus Yong <marcusyqy@gmail.com>
2024-10-07 00:41:55 -0400

    install.bat

--- install.bat
+++ install.bat
@@ -1,10 +1,16 @@
 @echo off
 
-rd /s /q %userprofile%\AppData\Local\nvim
-echo D | xcopy nvim %userprofile%\AppData\Local\nvim /E /A
+REM rd /s /q %userprofile%\AppData\Local\nvim
+REM echo D | xcopy nvim %userprofile%\AppData\Local\nvim /E /A
 
 REM echo A | xcopy alacritty %userprofile%\AppData\alacritty\ /E /A
 
 REM uncomment when you want to update wezterm config
 REM rd /s /q %userprofile%\.config\wezterm
 REM echo D | xcopy wezterm %userprofile%\.config\wezterm /E /A
+
+rd /s /q %userprofile%\AppData\Local\nvim
+mklink /d %userprofile%\AppData\Local\nvim %userprofile%\.dotfiles\nvim
+
+rd /s /q %userprofile%\.config\wezterm
+mklink /d %userprofile%\.config\wezterm %userprofile%\.dotfiles\wezterm
