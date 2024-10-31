506a3594a68922dc1967af0c26704cf6e3e07c86
Marcus Yong <marcusyqy@gmail.com>
2024-10-31 00:37:37 -0400

    Remove link.sh in favor of install.sh and trimmed it

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+link_nvim() {
+    vimconfig="$HOME/.config/nvim"
+    rm -rf $vimconfig
+    ln -s $DOTFILES/nvim/ $vimconfig
+}
+
+link_tmux() {
+    tmuxconfig="$HOME/.tmux.conf"
+    rm $tmuxconfig
+    ln -s $DOTFILES/tmux/.tmux.conf $tmuxconfig
+}
+
+
+link_wezterm() {
+    #[ -d "~/.config/alacritty" ] &&
+    weztermconfig="$HOME/.config/wezterm"
+    rm -rf $weztermconfig
+    ln -s $DOTFILES/wezterm $weztermconfig
+}
+
+link_nvim
+link_tmux
+link_wezterm
