a833bcd8259a7b08efa01be00d0640fc53949013
Marcus Yong <marcusyqy@gmail.com>
2024-11-01 00:50:26 -0400

    goodi

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -3,8 +3,9 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { "clangd", "lua_ls" } -- , "ocaml-lsp" }
+-- local servers = { "clangd", "lua_ls" } -- , "ocaml-lsp" }
 --[[ , 'rust_analyzer', 'pyright', 'tsserver', 'lua_ls', 'gopls', "svelte", "astro", "tailwindcss"  }]]
+ local servers = { "clangd", "emmet_ls", "lua_ls", "quick_lint_js", "svelte", "ts_ls" }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
