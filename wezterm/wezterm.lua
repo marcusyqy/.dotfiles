84733bd8260e65d8a1d4c3792aa7ff46094e83cb
Marcus Yong <marcusyqy@gmail.com>
2025-10-27 23:23:11 +0800

    Change keybinds

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,11 +14,11 @@ end
 --
 -- config.color_scheme = 'Rosé Pine (base16)'
 -- config.color_scheme = 'Rosé Pine (Gogh)'
-config.colors = {}
+-- config.colors = {}
 -- config.colors.background = '#1d1d1d'
-config.colors.foreground = '#ffffff'
+-- config.colors.foreground = '#ffffff'
 -- config.colors.background = '#1d2021'
-config.colors.background = '#000000' -- for paramount
+-- config.colors.background = '#000000' -- for paramount
 
 -- @TODO try this out on windows.
 config.default_cursor_style = "BlinkingBlock"
