87b0eca8572d35ccc0600820037dbae83c56b91f
Marcus Yong <marcusyqy@gmail.com>
2025-10-30 11:48:52 +0800

    Don't need the clangd extensions

--- nvim/init.lua
+++ nvim/init.lua
@@ -698,99 +698,99 @@ require("lazy").setup({
     { "tpope/vim-fugitive" },
     { "sainnhe/everforest" },
     { "ellisonleao/gruvbox.nvim" },
-    {
-      "p00f/clangd_extensions.nvim",
-      opts = {
-        server = {
-          -- options to pass to nvim-lspconfig
-          on_attach = on_attach,
-          cmd = {
-            "clangd",
-            "--header-insertion=never",
-            "--header-insertion-decorators",
-            "--function-arg-placeholders",
-            "--completion-style=detailed",
-            "--j=4",
-            "--pch-storage=memory",
-            "--compile-commands-dir=${workspaceFolder}/",
-            "--background-index",
-            -- "--clang-tidy=false",
-            "--suggest-missing-includes",
-            -- "--clang-tidy",
-            "--all-scopes-completion"
-            -- "--query-driver=**"
-            -- "query-driver=/**/*"
-          }
-        },
-        extensions = {
-          -- defaults:
-          autoSetHints = false,
-          -- These apply to the default ClangdSetInlayHints command
-          inlay_hints = {
-            -- Automatically set inlay hints (type hints)
-            inline = false, -- vim.fn.has("nvim-0.10") == 1,
-            -- Only show inlay hints for the current line
-            only_current_line = false,
-            -- Event which triggers a refersh of the inlay hints.
-            -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
-            -- not that this may cause  higher CPU usage.
-            -- This option is only respected when only_current_line and
-            -- autoSetHints both are true.
-            only_current_line_autocmd = "CursorHold",
-            -- whether to show parameter hints with the inlay hints or not
-            show_parameter_hints = true,
-            -- prefix for parameter hints
-            parameter_hints_prefix = "<- ",
-            -- prefix for all the other hints (type, chaining)
-            other_hints_prefix = "=> ",
-            -- whether to align to the length of the longest line in the file
-            max_len_align = false,
-            -- padding from the left if max_len_align is true
-            max_len_align_padding = 1,
-            -- whether to align to the extreme right or not
-            right_align = false,
-            -- padding from the right if right_align is true
-            right_align_padding = 7,
-            -- The color of the hints
-            highlight = "Comment",
-            -- The highlight group priority for extmark
-            priority = 100,
-          },
-          ast = {
-            role_icons = {
-              type = "",
-              declaration = "",
-              expression = "",
-              specifier = "",
-              statement = "",
-              ["template argument"] = "",
-            },
-            kind_icons = {
-              Compound = "",
-              Recovery = "",
-              TranslationUnit = "",
-              PackExpansion = "",
-              TemplateTypeParm = "",
-              TemplateTemplateParm = "",
-              TemplateParamObject = "",
-            },
-
-            highlights = {
-              detail = "Comment",
-            },
-          },
-          memory_usage = {
-            border = "none",
-          },
-          symbol_info = {
-            border = "none",
-          },
-        },
-      },
-      dependencies = {
-        "neovim/nvim-lspconfig",
-      },
-    },
+    -- {
+    --   "p00f/clangd_extensions.nvim",
+    --   opts = {
+    --     server = {
+    --       -- options to pass to nvim-lspconfig
+    --       on_attach = on_attach,
+    --       cmd = {
+    --         "clangd",
+    --         "--header-insertion=never",
+    --         "--header-insertion-decorators",
+    --         "--function-arg-placeholders",
+    --         "--completion-style=detailed",
+    --         "--j=4",
+    --         "--pch-storage=memory",
+    --         "--compile-commands-dir=${workspaceFolder}/",
+    --         "--background-index",
+    --         -- "--clang-tidy=false",
+    --         "--suggest-missing-includes",
+    --         -- "--clang-tidy",
+    --         "--all-scopes-completion"
+    --         -- "--query-driver=**"
+    --         -- "query-driver=/**/*"
+    --       }
+    --     },
+    --     extensions = {
+    --       -- defaults:
+    --       autoSetHints = false,
+    --       -- These apply to the default ClangdSetInlayHints command
+    --       inlay_hints = {
+    --         -- Automatically set inlay hints (type hints)
+    --         inline = false, -- vim.fn.has("nvim-0.10") == 1,
+    --         -- Only show inlay hints for the current line
+    --         only_current_line = false,
+    --         -- Event which triggers a refersh of the inlay hints.
+    --         -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+    --         -- not that this may cause  higher CPU usage.
+    --         -- This option is only respected when only_current_line and
+    --         -- autoSetHints both are true.
+    --         only_current_line_autocmd = "CursorHold",
+    --         -- whether to show parameter hints with the inlay hints or not
+    --         show_parameter_hints = true,
+    --         -- prefix for parameter hints
+    --         parameter_hints_prefix = "<- ",
+    --         -- prefix for all the other hints (type, chaining)
+    --         other_hints_prefix = "=> ",
+    --         -- whether to align to the length of the longest line in the file
+    --         max_len_align = false,
+    --         -- padding from the left if max_len_align is true
+    --         max_len_align_padding = 1,
+    --         -- whether to align to the extreme right or not
+    --         right_align = false,
+    --         -- padding from the right if right_align is true
+    --         right_align_padding = 7,
+    --         -- The color of the hints
+    --         highlight = "Comment",
+    --         -- The highlight group priority for extmark
+    --         priority = 100,
+    --       },
+    --       ast = {
+    --         role_icons = {
+    --           type = "",
+    --           declaration = "",
+    --           expression = "",
+    --           specifier = "",
+    --           statement = "",
+    --           ["template argument"] = "",
+    --         },
+    --         kind_icons = {
+    --           Compound = "",
+    --           Recovery = "",
+    --           TranslationUnit = "",
+    --           PackExpansion = "",
+    --           TemplateTypeParm = "",
+    --           TemplateTemplateParm = "",
+    --           TemplateParamObject = "",
+    --         },
+    --
+    --         highlights = {
+    --           detail = "Comment",
+    --         },
+    --       },
+    --       memory_usage = {
+    --         border = "none",
+    --       },
+    --       symbol_info = {
+    --         border = "none",
+    --       },
+    --     },
+    --   },
+    --   dependencies = {
+    --     "neovim/nvim-lspconfig",
+    --   },
+    -- },
     { "hrsh7th/nvim-cmp",
       dependencies = {
         { "hrsh7th/cmp-nvim-lua" },
@@ -965,6 +965,27 @@ require("lazy").setup({
         })
         vim.lsp.config("lua_ls", {
             on_attach = on_attach,
+        })
+        vim.lsp.config("clangd", {
+          on_attach = on_attach,
+          cmd = {
+            "clangd",
+            "--header-insertion=never",
+            "--header-insertion-decorators",
+            "--function-arg-placeholders",
+            "--completion-style=detailed",
+            "--j=4",
+            "--pch-storage=memory",
+            "--compile-commands-dir=${workspaceFolder}/",
+            "--background-index",
+            -- "--clang-tidy=false",
+            "--suggest-missing-includes",
+            -- "--clang-tidy",
+            "--all-scopes-completion"
+            -- "--query-driver=**"
+            -- "query-driver=/**/*"
+          }
+
         })
       end,
     },
