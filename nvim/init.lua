d7d63b2ea606f6486793c15aefd2397b451eef25
Marcus Yong <marcusyqy@gmail.com>
2025-12-11 01:04:25 +0800

    Adding neogit instead of fugitive. Let's see how it goes.

--- nvim/init.lua
+++ nvim/init.lua
@@ -599,7 +599,25 @@ require("lazy").setup({
     { "folke/lazydev.nvim" },
     { "nvim-lua/popup.nvim" },
     { "nvim-lua/plenary.nvim" },
-    { "tpope/vim-fugitive" },
+   -- { "tpope/vim-fugitive" },
+     {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",              -- optional
+    "nvim-mini/mini.pick",           -- optional
+    "folke/snacks.nvim",             -- optional
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+  }
+},
     { "sainnhe/everforest" },
     { "ellisonleao/gruvbox.nvim" },
     { "hrsh7th/nvim-cmp",
@@ -1223,8 +1241,12 @@ end
 vim.keymap.set("n", "<C-q>", function() ToggleQFList(true) end, { desc = "Open Global QF List" })
 vim.keymap.set("n", "<leader>q", function() ToggleQFList(false) end, { desc = "Open Local QF List" })
 
+-- vim.cmd([[
+--   cnoreabbrev g Git
+--   cnoreabbrev gopen GBrowse
+-- ]])
 vim.cmd([[
-  cnoreabbrev g Git
+  cnoreabbrev g Neogit
   cnoreabbrev gopen GBrowse
 ]])
 
