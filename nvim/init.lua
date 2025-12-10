fa17c7a31e2aab6826238a9d128190c9d186d5e0
Marcus Yong <marcusyqy@gmail.com>
2025-12-10 11:20:38 +0800

    Add help search

--- nvim/init.lua
+++ nvim/init.lua
@@ -271,6 +271,7 @@ require("lazy").setup({
         { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
         { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
         { "<c-p>", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+        { "<leader>fh", function() Snacks.picker.help() end, desc = "Search help files" },
         { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
         { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
         -- git
