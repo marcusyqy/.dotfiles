fd7505a4a526fadfe686554c86611a5a62e6155e
Marcus Yong <marcusyqy@gmail.com>
2026-01-18 01:34:11 +0800

    Change neovide config again

--- nvim/init.lua
+++ nvim/init.lua
@@ -1404,13 +1404,14 @@ if vim.g.neovide then
   vim.g.neovide_progress_bar_animation_speed = 200.0
   vim.g.neovide_progress_bar_hide_delay = 0.2
 
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_scroll_animation_length = 0.2
   vim.g.neovide_floating_shadow = true
   vim.g.neovide_floating_z_height = 10
   vim.g.neovide_light_angle_degrees = 45
   vim.g.neovide_light_radius = 5
 
   vim.g.neovide_cursor_animation_length = 0.1
-  vim.g.neovide_cursor_trail_size  = 0.2
-
+  vim.g.neovide_cursor_trail_size  = 0.1
 end
 
