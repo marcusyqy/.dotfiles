0e571e28b7e0352d93b3d6d56f47578f92bee65c
Marcus Yong <marcusyqy@gmail.com>
2026-01-21 10:54:59 +0800

    Remove font family

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -37,12 +37,12 @@ config.cursor_blink_rate = 500
 
 config.font_size = 13.0
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- no ligatures for me please!
-config.font = wezterm.font {
-    family = "Iosevka Nerd Font Mono", -- Or the font you prefer
-    -- family = "JetBrains Mono", -- Or the font you prefer
-    -- weight = "DemiBold", -- Optional: Adjust weight
-    harfbuzz_features = { "calt=0", "clig=0", "liga=0" }, -- Disable ligatures
-}
+-- config.font = wezterm.font {
+--     family = "Iosevka Nerd Font Mono", -- Or the font you prefer
+--     -- family = "JetBrains Mono", -- Or the font you prefer
+--     -- weight = "DemiBold", -- Optional: Adjust weight
+--     harfbuzz_features = { "calt=0", "clig=0", "liga=0" }, -- Disable ligatures
+-- }
 
 config.freetype_load_target = "HorizontalLcd"
 
