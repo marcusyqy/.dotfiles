31c23e876127fa1edbc2a9686f23ced08c459226
Marcus Yong <marcusyqy@gmail.com>
2026-02-27 19:40:07 +0800

    remove all themes, seems like the default themes are good enough for me

--- nvim/init.lua
+++ nvim/init.lua
@@ -140,104 +140,6 @@ end
 require("lazy").setup({
   spec = {
     {
-      "craftzdog/solarized-osaka.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        transparent = false, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "dark", -- style for floating windows
-        },
-        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false, -- dims inactive windows
-        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-        --- You can override specific color groups to use other groups or a hex color
-        --- function will be called with a ColorScheme table
-        ---@param colors ColorScheme
-        on_colors = function(colors) end,
-
-        --- You can override specific highlights to use other groups or a hex color
-        --- function will be called with a Highlights and ColorScheme table
-        ---@param highlights Highlights
-        ---@param colors ColorScheme
-        on_highlights = function(highlights, colors) end,
-      },
-    },
-    {
-      'olivercederborg/poimandres.nvim',
-      lazy = false,
-      priority = 1000,
-      config = function()
-        require('poimandres').setup {
-          -- bold_vert_split = false, -- use bold vertical separators
-          -- dim_nc_background = false, -- dim 'non-current' window backgrounds
-          disable_background = true, -- disable background
-          disable_float_background = true, -- disable background for floats
-          -- disable_italics = false, -- disable italics
-        }
-      end,
-    },
-    {
-      "zenbones-theme/zenbones.nvim",
-      dependencies = "rktjmp/lush.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.g.zenbones = { lightness = "dim" }
-      end
-    },
-    {
-      "srcery-colors/srcery-vim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.g.srcery_italic = 0
-      end
-    },
-    {
-      "ellisonleao/gruvbox.nvim",
-      lazy = false,
-      priority = 1000,
-      config = true,
-      opts = {
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "soft", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      },
-    },
-    {
       "folke/snacks.nvim",
       priority = 1000,
       lazy = false,
@@ -1417,7 +1319,7 @@ vim.keymap.set({"i", "n", "v", "t", "c" }, "<f4>", function() Snacks.terminal() 
 
 -- Set the background color of the entire cursor line
 vim.cmd.colorscheme("lunaperche")
-vim.api.nvim_set_hl(0, "Cursor", { bg = "#7c7c7c", fg = "NONE" })
+-- vim.api.nvim_set_hl(0, "Cursor", { bg = "#7c7c7c", fg = "NONE" })
 
 if vim.g.neovide then
     -- Put anything you want to happen only in Neovide here
