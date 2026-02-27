1c5072bc2272674b76326fa142490d6558c3aed5
Marcus Yong <marcusyqy@gmail.com>
2026-02-27 22:46:08 +0800

    remove statusline

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,7 +74,7 @@ vim.opt.smartindent = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
-vim.opt.laststatus = 3
+vim.opt.laststatus = 1
 vim.opt.showmode = true
 
 vim.opt.cursorline = false -- true
@@ -1037,64 +1037,64 @@ require("lazy").setup({
     { "evanleck/vim-svelte" },
     { "martinda/Jenkinsfile-vim-syntax" },
     { "kalvinpearce/ShaderHighlight" },
-    {
-      "nvim-lualine/lualine.nvim",
-      dependencies = { 'nvim-tree/nvim-web-devicons' },
-      opts = {
-        options = {
-          icons_enabled = true,
-          theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          always_show_tabline = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-            refresh_time = 16, -- ~60fps
-            events = {
-              'WinEnter',
-              'BufEnter',
-              'BufWritePost',
-              'SessionLoadPost',
-              'FileChangedShellPost',
-              'VimResized',
-              'Filetype',
-              'CursorMoved',
-              'CursorMovedI',
-              'ModeChanged',
-            },
-          }
-        },
-        sections = {
-          lualine_a = {'mode'}, --  {{'mode', fmt = function(res) return res:sub(1,1) end }},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
-          lualine_y = {},
-          lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-      }
-    },
+    -- {
+    --   "nvim-lualine/lualine.nvim",
+    --   dependencies = { 'nvim-tree/nvim-web-devicons' },
+    --   opts = {
+    --     options = {
+    --       icons_enabled = true,
+    --       theme = 'auto',
+    --       component_separators = { left = '', right = ''},
+    --       section_separators = { left = '', right = ''},
+    --       disabled_filetypes = {
+    --         statusline = {},
+    --         winbar = {},
+    --       },
+    --       ignore_focus = {},
+    --       always_divide_middle = true,
+    --       always_show_tabline = true,
+    --       globalstatus = false,
+    --       refresh = {
+    --         statusline = 1000,
+    --         tabline = 1000,
+    --         winbar = 1000,
+    --         refresh_time = 16, -- ~60fps
+    --         events = {
+    --           'WinEnter',
+    --           'BufEnter',
+    --           'BufWritePost',
+    --           'SessionLoadPost',
+    --           'FileChangedShellPost',
+    --           'VimResized',
+    --           'Filetype',
+    --           'CursorMoved',
+    --           'CursorMovedI',
+    --           'ModeChanged',
+    --         },
+    --       }
+    --     },
+    --     sections = {
+    --       lualine_a = {'mode'}, --  {{'mode', fmt = function(res) return res:sub(1,1) end }},
+    --       lualine_b = {'branch', 'diff', 'diagnostics'},
+    --       lualine_c = {'filename'},
+    --       lualine_x = {'encoding', 'fileformat', 'filetype'},
+    --       lualine_y = {'progress'},
+    --       lualine_z = {'location'}
+    --     },
+    --     inactive_sections = {
+    --       lualine_a = {},
+    --       lualine_b = {},
+    --       lualine_c = {'filename'},
+    --       lualine_x = {'location'},
+    --       lualine_y = {},
+    --       lualine_z = {}
+    --     },
+    --     tabline = {},
+    --     winbar = {},
+    --     inactive_winbar = {},
+    --     extensions = {}
+    --   }
+    -- },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
