ed89d6c619c27295699c511a2c8f6f51b18a6ae3
Marcus Yong <marcusyqy@gmail.com>
2026-03-18 20:34:09 +0800

    lualine change

--- nvim/init.lua
+++ nvim/init.lua
@@ -1105,20 +1105,7 @@ require("lazy").setup({
         'nvim-tree/nvim-web-devicons' ,
         'arkav/lualine-lsp-progress'
       },
-      config = function()
-        -- Color for highlights
-        local colors = {
-          yellow = '#ECBE7B',
-          cyan = '#008080',
-          darkblue = '#081633',
-          green = '#98be65',
-          orange = '#FF8800',
-          violet = '#a9a1e1',
-          magenta = '#c678dd',
-          blue = '#51afef',
-          red = '#ec5f67'
-        }
-        local config = {
+      opts = {
           options = {
             icons_enabled = true,
             theme = 'auto',
@@ -1154,7 +1141,7 @@ require("lazy").setup({
           sections = {
             lualine_a = {'mode'}, --  {{'mode', fmt = function(res) return res:sub(1,1) end }},
             lualine_b = {'branch', 'diff', 'diagnostics'},
-            lualine_c = {'filename', 'lsp_progress'},
+            lualine_c = {'filename', { 'lsp_progress',  spinner_symbols = {'⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷'}, }, },
             lualine_x = {'encoding', 'fileformat', 'filetype'},
             lualine_y = {'progress'},
             lualine_z = {'location'}
@@ -1172,45 +1159,6 @@ require("lazy").setup({
           inactive_winbar = {},
           extensions = {}
         }
-        -- Inserts a component in lualine_c at left section
-        local function ins_left(component)
-          table.insert(config.sections.lualine_c, component)
-        end
-
-        -- Inserts a component in lualine_x ot right section
-        local function ins_right(component)
-          table.insert(config.sections.lualine_x, component)
-        end
-
-        ins_left {
-          'lsp_progress',
-          display_components = { 'lsp_client_name', { 'title', 'percentage', 'message' }},
-          -- With spinner
-          -- display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' }},
-          colors = {
-            percentage  = colors.cyan,
-            title  = colors.cyan,
-            message  = colors.cyan,
-            spinner = colors.cyan,
-            lsp_client_name = colors.magenta,
-            use = true,
-          },
-          separators = {
-            component = ' ',
-            progress = ' | ',
-            message = { pre = '(', post = ')'},
-            percentage = { pre = '', post = '%% ' },
-            title = { pre = '', post = ': ' },
-            lsp_client_name = { pre = '[', post = ']' },
-            spinner = { pre = '', post = '' },
-            message = { commenced = 'In Progress', completed = 'Completed' },
-          },
-          display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
-          timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
-          spinner_symbols = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ' },
-        }
-        require('lualine').setup(config)
-      end
     },
   },
   -- Configure any other settings here. See the documentation for more details.
