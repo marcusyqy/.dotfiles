341fd6053f7d50d2da8893e30b9bd6c200d1ca94
Marcus Yong <marcusyqy@gmail.com>
2026-04-21 03:04:22 +0800

    not sure if this does anything though

--- nvim/init.lua
+++ nvim/init.lua
@@ -146,6 +146,7 @@ require("lazy").setup({
       config = function()
         vim.g.gruvbox_material_enable_italic = false
         vim.g.gruvbox_material_foreground = "mixed" -- "original"
+        vim.g.gruvbox_material_ui_contrast = "high"
         vim.g.gruvbox_material_statusline_style= "mixed" -- "original"
         vim.g.gruvbox_material_background = "hard"
         vim.g.gruvbox_material_transparent_background = false
