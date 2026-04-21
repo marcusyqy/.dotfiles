ce263fc8cbae1a7264a74da6b55b0643aa5dca13
Marcus Yong <marcusyqy@gmail.com>
2026-04-22 01:34:05 +0800

    commiting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1203,6 +1203,7 @@ if vim.g.neovide then
   vim.g.neovide_light_angle_degrees = 45
   vim.g.neovide_light_radius = 5
 
+  vim.g.neovide_cursor_short_animation_length = 0.02
   vim.g.neovide_cursor_animation_length = 0.05
   vim.g.neovide_cursor_trail_size  = 0.05
 end
