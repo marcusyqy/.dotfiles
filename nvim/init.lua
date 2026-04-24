20b5b0d3d49f742489b0a780d02434c820bdc9b4
Marcus Yong <marcusyqy@gmail.com>
2026-04-24 21:04:37 +0800

    inconsistency

--- nvim/init.lua
+++ nvim/init.lua
@@ -1218,7 +1218,7 @@ if vim.g.neovide then
     vim.g.neovide_progress_bar_hide_delay = 0.2
     vim.g.neovide_refresh_rate = 60
 
-    vim.g.neovide_hide_mouse_when_typing = true
+    vim.g.neovide_hide_mouse_when_typing = false
     vim.g.neovide_scroll_animation_length = 0.1
     vim.g.neovide_floating_shadow = true
     vim.g.neovide_floating_z_height = 10
