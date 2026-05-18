4b64eda5fc6dfd1f1e26de81d6b44f8bf2bef7f9
Marcus Yong <marcusyqy@gmail.com>
2026-05-19 00:25:32 +0800

    add more colorschemes

--- nvim/init.lua
+++ nvim/init.lua
@@ -183,18 +183,45 @@ require("lazy").setup({
      end
     },
     {
+      "craftzdog/solarized-osaka.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = false },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+      },
+    },
+    {
       "folke/tokyonight.nvim",
       lazy = false,
       priority = 1000,
       opts = {
-        style = "storm",
+        style = "night",
         transparent = false,
         terminal_colors = true,
         styles = {
           -- Style to be applied to different syntax groups
           -- Value is any valid attr-list value for `:help nvim_set_hl`
           comments = { italic = true },
-          keywords = {},
+          keywords = { italic = false },
           functions = {},
           variables = {},
           -- Background styles. Can be "dark", "transparent" or "normal"
@@ -308,6 +335,16 @@ require("lazy").setup({
           }
         })
       end
+    },
+    {
+      "vague-theme/vague.nvim",
+      priority = 1000,
+      lazy = false,
+      opts = {
+        transparent = false, -- If true, background is not set
+        bold = true, -- Disable bold globally
+        italic = true, -- Disable italic globally
+      },
     },
     {
       "sainnhe/everforest",
@@ -1510,7 +1547,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 })
 
 
-vim.cmd.colorscheme("tokyonight")
+vim.cmd.colorscheme("vague")
 
 -- vim.cmd([[
 -- colorscheme default
