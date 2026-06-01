dd9a426af3a8d9cc6c4342cf8412179ab60ba104
Marcus Yong <marcusyqy@gmail.com>
2026-06-01 22:14:39 +0800

    trying out darker color again

--- nvim/init.lua
+++ nvim/init.lua
@@ -335,7 +335,7 @@ require("lazy").setup({
           -- style = 'darker'
           -- Lua
           -- Main options --
-          style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+          style = 'darker', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
           transparent = false,  -- Show/hide background
           term_colors = true, -- Change terminal color as per the selected theme style
           ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
@@ -372,6 +372,226 @@ require("lazy").setup({
             background = true,    -- use background color for virtual text
           },
         }
+      end
+    },
+    {
+      "scottmckendry/cyberdream.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function ()
+        require("cyberdream").setup({
+          -- Set light or dark variant
+          variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
+
+          -- Enable transparent background
+          transparent = false,
+
+          -- Reduce the overall saturation of colours for a more muted look
+          saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
+
+          -- Enable italics comments
+          italic_comments = true,
+
+          -- Replace all fillchars with ' ' for the ultimate clean look
+          hide_fillchars = false,
+
+          -- Apply a modern borderless look to pickers like Telescope, Snacks Picker & Fzf-Lua
+          borderless_pickers = true,
+
+          -- Set terminal colors used in `:terminal`
+          terminal_colors = true,
+
+          -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
+          cache = false,
+
+          -- Override highlight groups with your own colour values
+          highlights = {
+            -- Highlight groups to override, adding new groups is also possible
+            -- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+            -- Example:
+            -- Comment = { fg = "#696969", bg = "NONE", italic = true },
+
+            -- More examples can be found in `lua/cyberdream/extensions/*.lua`
+          },
+
+          -- Override a highlight group entirely using the built-in colour palette
+          -- overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
+          --   -- Example:
+          --   return {
+          --     Comment = { fg = colors.green, bg = "NONE", italic = true },
+          --     ["@property"] = { fg = colors.magenta, bold = true },
+          --   }
+          -- end,
+
+          -- Override colors
+          -- colors = {
+          --   -- For a list of colors see `lua/cyberdream/colours.lua`
+          --
+          --   -- Override colors for both light and dark variants
+          --   bg = "#000000",
+          --   green = "#00ff00",
+          --
+          --   -- If you want to override colors for light or dark variants only, use the following format:
+          --   dark = {
+          --     magenta = "#ff00ff",
+          --     fg = "#eeeeee",
+          --   },
+          --   light = {
+          --     red = "#ff5c57",
+          --     cyan = "#5ef1ff",
+          --   },
+          -- },
+
+          -- Disable or enable colorscheme extensions
+          extensions = {
+            telescope = true,
+            notify = true,
+            mini = true,
+          },
+
+          -- Alternatively, you can use 'default' to set all extensions at once
+          -- cache = true, -- Use cache for fastest loads
+          -- extensions = {
+          --     default = false, -- Disable all by default
+          --     base = true, -- Enable all built-in hl groups (you probably want this)
+          --
+          --     -- Now enable only what you want to use
+          --     telescope = true,
+          --     cmp = true,
+          --     gitsigns = true,
+          -- },
+        })
+      end
+    },
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {
+        style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+        light_style = "day", -- The theme is used when the background is set to light
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights tokyonight.Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
+
+        cache = true, -- When set to true, the theme will be cached for better performance
+
+        ---@type table<string, boolean|{enabled:boolean}>
+        plugins = {
+          -- enable all plugins when not using lazy.nvim
+          -- set to false to manually enable/disable plugins
+          all = package.loaded.lazy == nil,
+          -- uses your plugin manager to automatically enable needed plugins
+          -- currently only lazy.nvim is supported
+          auto = true,
+          -- add any plugins here that you want to enable
+          -- for all possible plugins, see:
+          --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+          -- telescope = true,
+        },
+      },
+    },
+    {
+      "catppuccin/nvim",
+      priority = 1000,
+      name = "catppuccin",
+      lazy = false,
+      config = function()
+        require("catppuccin").setup({
+          flavour = "auto", -- latte, frappe, macchiato, mocha
+          background = { -- :h background
+            light = "latte",
+            dark = "mocha",
+          },
+          transparent_background = false, -- disables setting the background color.
+          float = {
+            transparent = false, -- enable transparent floating windows
+            solid = false, -- use solid styling for floating windows, see |winborder|
+          },
+          term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+          dim_inactive = {
+            enabled = false, -- dims the background color of inactive window
+            shade = "dark",
+            percentage = 0.15, -- percentage of the shade to apply to the inactive window
+          },
+          no_italic = false, -- Force no italic
+          no_bold = false, -- Force no bold
+          no_underline = false, -- Force no underline
+          styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+            comments = { "italic" }, -- Change the style of comments
+            conditionals = { "italic" },
+            loops = {},
+            functions = {},
+            keywords = {},
+            strings = {},
+            variables = {},
+            numbers = {},
+            booleans = {},
+            properties = {},
+            types = {},
+            operators = {},
+            -- miscs = {}, -- Uncomment to turn off hard-coded styles
+          },
+          lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+              ok = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+              ok = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          color_overrides = {},
+          custom_highlights = {},
+          default_integrations = true,
+          auto_integrations = false,
+          integrations = {
+            cmp = true,
+            gitsigns = true,
+            nvimtree = true,
+            notify = false,
+            mini = {
+              enabled = true,
+              indentscope_color = "",
+            },
+            -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+          },
+        })
       end
     },
     {
@@ -630,10 +850,10 @@ require("lazy").setup({
         local ensureInstalled = { "c", "cpp", "lua" }
         local alreadyInstalled = require('nvim-treesitter.config').get_installed()
         local parsersToInstall = vim.iter(ensureInstalled)
-		:filter(function(parser)
-		  return not vim.tbl_contains(alreadyInstalled, parser)
-		end)
-		:totable()
+          :filter(function(parser)
+            return not vim.tbl_contains(alreadyInstalled, parser)
+          end)
+          :totable()
         require('nvim-treesitter').install(parsersToInstall)
       end,
     },
@@ -1490,7 +1710,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 })
 
 
-vim.cmd.colorscheme("0x96f")
+vim.cmd.colorscheme("cyberdream")
 
 -- vim.cmd([[
 -- colorscheme default
